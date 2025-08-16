afaf2ddfd0486e3c5f49f45f7878619ba79ce440
Khalid Deniev <haliddeniev88@gmail.com>
2025-08-16 21:35:44 +0300

    develop

--- src/entity/field.ts
+++ src/entity/field.ts
@@ -1,14 +1,14 @@
-import type { SchemaDefinitionType } from 'mongoose'
+import type { SchemaDefinitionType, SchemaTypes } from 'mongoose'
 import type { FieldAttribute } from '../types'
 
 export class Field {
   /** The name of the field */
   private readonly name: string
 
-  /** The maximum value allowed for the field  */
+  /** The maximum value allowed for the field (if applicable) */
   private readonly max?: number
 
-  /** The minimum value allowed for the field */
+  /** The minimum value allowed for the field (if applicable) */
   private readonly min?: number
 
   /** Whether the field is required */
@@ -16,6 +16,24 @@ export class Field {
 
   /** The Mongoose schema type for the field */
   private type?: SchemaDefinitionType<any>
+
+  /** The default value for the field */
+  private default?: any
+
+  /** Enumeration of allowed values */
+  private enum?: any[]
+
+  /** Whether the field must be unique */
+  private unique?: boolean
+
+  /** Whether the field is indexed */
+  private index?: boolean
+
+  /** Whether the field is immutable */
+  private immutable?: boolean
+
+  /** Whether the field should be selected by default */
+  private select?: boolean
 
   /**
    * Creates a new Field instance with the given name.
@@ -80,6 +98,60 @@ export class Field {
    */
   getType(): SchemaDefinitionType<any> {
     return this.type
+  }
+
+  /**
+   * Returns the default value of the field, if any.
+   *
+   * @returns The default value or undefined.
+   */
+  getDefault(): any {
+    return this.default
+  }
+
+  /**
+   * Returns the allowed enumeration values for the field, if any.
+   *
+   * @returns An array of allowed values or undefined.
+   */
+  getEnum(): any[] | undefined {
+    return this.enum
+  }
+
+  /**
+   * Returns whether the field must be unique.
+   *
+   * @returns True if the field is unique, false or undefined otherwise.
+   */
+  getUnique(): boolean | undefined {
+    return this.unique
+  }
+
+  /**
+   * Returns whether the field is indexed.
+   *
+   * @returns True if the field has an index, false or undefined otherwise.
+   */
+  getIndex(): boolean | undefined {
+    return this.index
+  }
+
+  /**
+   * Returns whether the field is immutable.
+   *
+   * @returns True if immutable, false or undefined otherwise.
+   */
+  getImmutable(): boolean | undefined {
+    return this.immutable
+  }
+
+  /**
+   * Returns whether the field should be selected by default.
+   *
+   * @returns True if selected by default, false or undefined otherwise.
+   */
+  getSelect(): boolean | undefined {
+    return this.select
   }
 
   /**
