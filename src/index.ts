e06c64b8b11e0ffdb697fc9a71c63b2df8d35b14
Khalid Deniev <haliddeniev88@gmail.com>
2025-08-18 15:23:02 +0300

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-import mongoose from 'mongoose'
-import { builder } from './builders'
-
-type User = { age: number }
-
-const User = builder<User>('User', (builder) => [
-  builder.field('age').number(),
-]).build()
-
-mongoose.connect('mongodb://localhost:27017/mydatabase').then(async () => {
-  await User
-})
+export { builder } from './builders'
